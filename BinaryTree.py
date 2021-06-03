4200aeab72efe4d2c8d1ad07fcc2fbf9def191bf
Adi Peled <Adidsdog@gmail.com>
2021-06-03 15:37:14 +0300

    made members public because it's a standard binary tree

--- BinaryTree.py
+++ BinaryTree.py
@@ -3,21 +3,25 @@ class BinaryNode:
     RIGHT_DIRECTION = 1
 
     def __init__(self, value):
-        self._key = value
-        self._left_son = None
-        self._right_son = None
+        self.key = value
+        self.left_son = None
+        self.right_son = None
 
     def make_son(self, node, direction):
         if direction == BinaryNode.LEFT_DIRECTION:
-            self._left_son = node
+            self.left_son = node
         elif direction == BinaryNode.RIGHT_DIRECTION:
-            self._right_son = node
+            self.right_son = node
+
+    def _make_sons(self, lnode, rnode):
+        self.left_son = lnode
+        self.right_son = rnode
 
     def get_son(self, direction):
         node = None
         if direction == BinaryNode.LEFT_DIRECTION:
-            node = self._left_son
+            node = self.left_son
         elif direction == BinaryNode.RIGHT_DIRECTION:
-            node = self._right_son
+            node = self.right_son
         return node
 
