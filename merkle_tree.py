ee69f4975245410aefef80dc29c8fd8c70662a99
ItamarFi <itamar.fisch@gmail.com>
2021-06-04 11:36:25 +0300

    complete data structure + inorder print for debug

--- merkle_tree.py
+++ merkle_tree.py
@@ -3,28 +3,101 @@ import math
 import base64
 from hashlib import sha256
 
-merkle_tree = None
-sparse_merkle_tree = None
+
+class Node :
+    def __init__(self, data) :
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.data = str(data)
+
+    def inorder_print(self):
+        if self is not None:
+            print(self.data)
+            if self.left is not None:
+                self.left.inorder_print()
+                self.right.inorder_print()
+
+def left_binding(parent, child):
+    parent.left = child
+    child.parent = parent
 
 
-def print_error(msg):
+class BinaryMerkleTree :
+    def __init__(self) :
+        self.root = None
+        self.leaves = []
+
+    def add_leaf(self, node):
+        self.leaves.append(node)
+        if len(self.leaves) == 1:
+            self.root = node
+            return 1
+
+        temp = Node(0)
+        right = self.leaves[len(self.leaves) - 1]
+
+        # replace root
+        if math.log(len(self.leaves) - 1, 2).is_integer():
+            self.triangle_binding(temp, self.root, right)
+            self.root = temp
+        elif len(self.leaves) % 2 == 0:
+            left = self.leaves[len(self.leaves) - 2]
+            grandpa = left.parent
+            self.triangle_binding(temp, left, right)
+            self.right_binding(grandpa, temp)
+        else:
+            left = self.root.right
+            self.triangle_binding(temp, left, right)
+            self.right_binding(self.root, temp)
+        self.node_diffusion(node.parent)
+
+    def node_diffusion(self, node):
+        if node is not None:
+            self.hash_calculator(node)
+            self.node_diffusion(node.parent)
+
+    def right_binding(self, parent, child):
+        parent.right = child
+        child.parent = parent
+
+    def triangle_binding(self, parent, left, right):
+        left_binding(parent, left)
+        self.right_binding(parent, right)
+
+    def hash_calculator(self, parent):
+        data1 = parent.left.data
+        data2 = parent.right.data
+        parent.data = "(" + data1 + ")+(" + data2 + ")"
+
+    def inorder_traversal(self):
+        self.root.inorder_print()
+
+
+def print_error(msg) :
     # TODO before submit make sure to replace the code with "print('')"
     # This function will help us make all the prints disappear before submit
     print(msg)
 
-
 # This function is wrong! Listen to your recording with the improvements
-def calculate_max_needed_values_for_sparse_tree():
-    num_hashes_in_tree_level = [0] * 257
-    nodes_in_level = 2 ** 256
-    num_possible_values_in_level = 2
-    for i in range(-1, -len(num_hashes_in_tree_level) - 1, -1):
-        if num_possible_values_in_level < nodes_in_level:
-            num_hashes_in_tree_level[i] = num_possible_values_in_level
-            num_possible_values_in_level *= num_possible_values_in_level
-        else:
-            num_hashes_in_tree_level[i] = nodes_in_level
-        nodes_in_level >>= 1
-    return num_hashes_in_tree_level
+# def calculate_max_needed_values_for_sparse_tree():
+#     num_hashes_in_tree_level = [0] * 257
+#     nodes_in_level = 2 ** 256
+#     num_possible_values_in_level = 2
+#     for i in range(-1, -len(num_hashes_in_tree_level) - 1, -1):
+#         if num_possible_values_in_level < nodes_in_level:
+#             num_hashes_in_tree_level[i] = num_possible_values_in_level
+#             num_possible_values_in_level *= num_possible_values_in_level
+#         else:
+#             num_hashes_in_tree_level[i] = nodes_in_level
+#         nodes_in_level >>= 1
+#     return num_hashes_in_tree_level
 
 
+merkle_tree = BinaryMerkleTree()
+
+for x in range(8):
+    node = Node(x)
+    merkle_tree.add_leaf(node)
+
+merkle_tree.inorder_traversal()
