342a9692ca200b04d5f656e1385f5164d2e488eb
Adi Peled <Adidsdog@gmail.com>
2021-06-07 16:18:10 +0300

    Fixed "generate_proof_of_inclusion" to fit the course auto check format. This means: the proof doesn't include hashes that can be computed by: h0=get_leaf(digest).key, hi+1=hash(hi||hi).

--- SparseMerkleTree.py
+++ SparseMerkleTree.py
@@ -9,6 +9,16 @@ def print_error(msg):
     print(msg)
 
 
+class MerkleNode(BinaryNode):
+    def get_route_to_leaf(self, leaf_num):
+        route = []
+        next_node = self
+        while next_node is not None:
+            route.append(next_node)
+
+            next_node = next_node.get_son()
+
+
 class SparseMerkleTree:
     def __init__(self, depth: int):
         # for 2**256 leaves we need depth of 256
@@ -16,9 +26,21 @@ class SparseMerkleTree:
         curr = MerkleNode(0)
         while depth > 0:
             depth -= 1
-            curr_parent = BinaryNode(0, curr, curr)
+            curr_parent = MerkleNode(0, curr, curr)
             curr = curr_parent
         self.root = curr
+
+    def _get_route_to_leaf(self, digest):
+        route = []
+        mask = 1 << (self.depth - 1)
+        path_int = int(digest, 8)
+        next_node = self.root
+        while next_node is not None:
+            route.append(next_node)
+            direction = (path_int & mask) != 0  # direction is 1 iff the next direction bit is 1
+            mask = mask >> 1
+            next_node = next_node.get_son(direction)
+        return route
 
     def mark_leaf(self, digest):
         SparseMerkleTree.mark_leaf.bit_to_int_offset = ord('0')
@@ -55,13 +77,28 @@ class SparseMerkleTree:
         return self.root.get_key()
 
     def generate_proof_of_inclusion(self, digest):
-        leaf_number = int(digest, 2)
-        regular_proof = self.root.generate_proof_of_inclusion(leaf_number)
-        # removing the direction bit
-        first_key = regular_proof.pop(0)
-        proof = [key[1:] for key in regular_proof]
-        proof.insert(0, first_key)
-        return proof
+        # initialize
+        root_sign = self.root.compute_key()
+        proof = []
+        route_to_digest = self._get_route_to_leaf(digest)
+
+        # skip nodes that can be computed using only the digest value
+        while len(route_to_digest) > 1 and route_to_digest[-2].left_son.key == route_to_digest[-2].right_son.key:
+            route_to_digest.pop(-1)
+        # if at least one node was removed, append the last hash of digest that is the same as the hash of its sibling
+        if len(route_to_digest) < self.depth:
+            proof.append(route_to_digest[-1].key)
+
+        # create the rest of the proof
+        for i in range(len(route_to_digest) - 2, -1, -1):
+            # if the left son is on the path to digest, add the other sibling to the proof
+            if route_to_digest[i].left_son is route_to_digest[i + 1]:
+                proof.append(route_to_digest[i].right_son.key)
+            else:
+                proof.append(route_to_digest[i].left_son.key)
+
+        # return proof
+        return root_sign, proof
 
     def check_proof_of_inclusion(self, digest, classification_bit, proof):
         # adding the direction bit to each key
